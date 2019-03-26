693dbcd5488fe865538d982ac10b094b46147dea
erikarvidsson <erik.arvidsson.89@gmail.com>
2019-03-27 00:09:35 +0100

    Updates

---
+++ precache-manifest.347c580ca2f7fdd701edd35477359067.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "b444657a8bb6cf2ead75",
+    "url": "/static/css/main.8edf3ec1.chunk.css"
+  },
+  {
+    "revision": "b444657a8bb6cf2ead75",
+    "url": "/static/js/main.88f230a5.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "002b578d8d66b88f6b29",
+    "url": "/static/js/2.9a23645d.chunk.js"
+  },
+  {
+    "revision": "80361a0046fa98e16b79db8826e2944e",
+    "url": "/index.html"
+  }
+];
